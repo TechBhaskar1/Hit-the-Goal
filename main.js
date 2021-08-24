9439dafd0738d445dd409869efed6d1fad4d7264
TechBhaskar1 <81464944+TechBhaskar1@users.noreply.github.com>
2021-08-24 06:34:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,120 @@
+var canvas=new fabric.Canvas('myCanvas');
+
+ballX=0;
+ballY=0;
+holeY=400;
+holeX=800;
+
+block_image_width = 5;
+block_image_height = 5;
+
+function load_img(){
+	fabric.Image.fromURL("golf-h.png",function(Img){
+		hole_obj=Img;
+		hole_obj.scaleToWidth(50);
+		hole_obj.scaleToHeight(50);
+		hole_obj.set({
+			top:holeY,
+			left:holeX
+		});
+		canvas.add(hole_obj);
+		});
+	new_image();
+}
+
+function new_image()
+{
+	fabric.Image.fromURL("ball.png",function(Img){
+		ball_obj=Img;
+		ball_obj.scaleToWidth(50);
+		ball_obj.scaleToHeight(50);
+		ball_obj.set({
+			top:ballY,
+			left:ballX
+		});
+		canvas.add(ball_obj);
+		});
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+
+	if((ballX==holeX) && (ballY==holeY)){
+		canvas.remove(ball_obj);
+		document.getElementById("hd3").innerHTML="You hit the GOAL!!!!";
+		document.getElementById("myCanvas").style.borderColor="red";
+	}
+	
+	else{
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+	}
+	function up() 
+{
+    if(ballY>=0)
+    {
+        ballY=ballY-block_image_height;
+        console.log("Block height ="+block_image_height);
+        console.log("position x ="+ballX + "position y="+ballY);
+        canvas.remove(ball_obj);
+        new_image();
+    }
+}
+function down()
+{
+    if(ballY<=450)
+    {
+        ballY=ballY+block_image_height;
+        console.log("Block height ="+block_image_height);
+        console.log("position x ="+ballX + "position y="+ballY);
+        canvas.remove(ball_obj);
+        new_image();
+    }
+}
+function left()
+{
+    if(ballX>=0)
+    {
+        ballX=ballX-block_image_width;
+        console.log("Block height ="+block_image_width);
+        console.log("position x ="+ballX + "position y="+ballY);
+        canvas.remove(ball_obj);
+        new_image();
+    }
+}
+function right()
+{
+    if(ballX<=900)
+    {
+		ballX=ballX+block_image_width;
+        console.log("Block height ="+block_image_width);
+        console.log("position x ="+ballX + "position y="+ballY);
+        canvas.remove(ball_obj);
+        new_image();
+    }
+}
+	
+}
+
